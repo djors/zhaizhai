e914a4f896596e5bb5cbb811479b43a85f3f115c
djors <843971547@qq.com>
2025-03-31 21:31:46 +0800

    feat:zhaizhai weAppServer init

---
+++ weapp-server/src/main/java/com/zhaizhai/weappserver/WeAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.zhaizhai.weappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeAppServerApplication.class, args);
+    }
+
+}
---
+++ weapp-server/src/test/java/com/zhaizhai/weappserver/WeAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaizhai.weappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
